3d406ce1091d119c1d8b547f3d0b08b7301cce77
Jordan Hansen <jbhansen84@gmail.com>
2020-08-07 13:15:02 -0600

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import puppeteer from 'puppeteer';
+
+(async () => {
+
+	for (let i = 0; i < 10; i++) {
+		const browser = await puppeteer.launch({ headless: false });
+		const page = await browser.newPage();
+
+		await page.goto('https://www.zillow.com/homedetails/2189-Gayle-Ave-Memphis-TN-38127/42228852_zpid/');
+
+		await page.waitFor(2500);
+
+
+		await browser.close();
+	}
+
+
+})();
