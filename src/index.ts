3cb3ce2eba5d6021f4eb1086fc0cdb3b3da136ac
Jordan Hansen <jbhansen84@gmail.com>
2020-08-07 13:23:46 -0600

    add duckduckgo first

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,8 @@ import puppeteer from 'puppeteer';
 		const context = await browser.createIncognitoBrowserContext();
 		const incognitoPage = await context.newPage();
 
+		await incognitoPage.goto('https://duckduckgo.com/?q=2189+gayle+ave+memphis&ia=maps');
+
 		await incognitoPage.goto('https://www.zillow.com/homedetails/2189-Gayle-Ave-Memphis-TN-38127/42228852_zpid/');
 
 		await incognitoPage.waitFor(2500);
