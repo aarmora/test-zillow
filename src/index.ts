ebf323bc2ac934c75543137c43beb58fd5b64422
Jordan Hansen <jbhansen84@gmail.com>
2020-08-07 13:17:02 -0600

    no sandbox

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,12 @@ import puppeteer from 'puppeteer';
 (async () => {
 
 	for (let i = 0; i < 10; i++) {
-		const browser = await puppeteer.launch({ headless: false });
+		const browser = await puppeteer.launch({
+			headless: false,
+			args: [
+				'--no-sandbox'
+			]
+		});
 		const page = await browser.newPage();
 
 		await page.goto('https://www.zillow.com/homedetails/2189-Gayle-Ave-Memphis-TN-38127/42228852_zpid/');
