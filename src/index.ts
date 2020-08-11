a8743d4ffbc2476264b93fab0ea875e5f7ca7a7a
Jordan Hansen <jbhansen84@gmail.com>
2020-08-11 06:48:16 -0600

    add stuff

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import puppeteer from 'puppeteer';
+import puppeteer, { Page } from 'puppeteer';
 
 const puppeteerExtra = require('puppeteer-extra');
 const pluginStealth = require('puppeteer-extra-plugin-stealth');
@@ -19,20 +19,48 @@ const pluginStealth = require('puppeteer-extra-plugin-stealth');
 
 		await incognitoPage.goto('https://duckduckgo.com/?q=2189+gayle+ave+memphis&ia=maps');
 
+		await incognitoPage.setRequestInterception(true);
+		incognitoPage.on('request', (request) => {
+			if (request.resourceType() === 'image') {
+				request.abort();
+			}
+			else request.continue();
+		});
+
 		await incognitoPage.goto('https://www.zillow.com/homedetails/2189-Gayle-Ave-Memphis-TN-38127/42228852_zpid/');
 
+		// We need to get the url because that should for sure have the zpid
+		const splitUrl = (await incognitoPage.url()).split('/');
+		let zpid = '';
+
+		for (let i = 0; i < splitUrl.length; i++) {
+			if (splitUrl[i].includes('zpid')) {
+				zpid = splitUrl[i].replace('_zpid', '');
+			}
+		}
+
 		try {
-			await incognitoPage.waitForSelector('title', { timeout: 2500 });
+			await incognitoPage.waitForSelector('.error-content-block', { timeout: 750 });
+
+			console.log('Looks like we hit reCaptcha in zillow search');
+
+			continue;
 		}
 		catch (e) {
-			console.log('No title found');
-			await incognitoPage.close();
+
+		}
+
+
+		let property: any;
+		try {
+			property = await fetchZillowPropertyData(parseInt(zpid), incognitoPage);
+		}
+		catch (e) {
+			console.log('error fetching zillow from zillow.', e);
 			continue;
 		}
 
-		const title = await incognitoPage.$eval('title', element => element.textContent);
-
-		console.log('title', title);
+		console.log('property', property)
 
 		await incognitoPage.close();
 	}
@@ -40,3 +68,41 @@ const pluginStealth = require('puppeteer-extra-plugin-stealth');
 	await browser.close();
 
 })();
+
+
+export async function fetchZillowPropertyData(zpid: number, page: Page, proxy = false) {
+	const payload = {
+		"operationName": "OffMarketFullRenderQuery",
+		"variables": {
+			"zpid": zpid,
+			"contactFormRenderParameter": {
+				"zpid": zpid,
+				"platform": "desktop",
+				"isDoubleScroll": false
+			}
+		},
+		"clientVersion": "home-details/6.0.11.400.master.a1941d1",
+		"queryId": "38fa1638c8a2fc3dc21a8ee73518f240"
+	};
+	let fetchUrl = `https://www.zillow.com/graphql/?zpid=${zpid}&contactFormRenderParameter=&queryId=38fa1638c8a2fc3dc21a8ee73518f240&operationName=OffMarketFullRenderQuery`;
+	if (proxy) {
+		fetchUrl = `http://api.scrapestack.com/scrape?access_key=${process.env.scrapeStackApiKey}&url=${fetchUrl}`;
+	}
+
+	const propertyData = await page.evaluate(async (payload: any, fetchUrl: string) => {
+		console.log('fetch url', fetchUrl);
+
+		const response = await fetch(fetchUrl, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify(payload)
+		});
+
+		return await response.json();
+
+	}, payload, fetchUrl);
+
+	return propertyData.data.property;
+}
